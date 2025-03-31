741201f76bebcf6c1b973832ba33f7e4a7cfe366
Monksoffunk <monksoffunk@users.noreply.github.com>
2025-03-31 14:50:10 +0900

    Add Vial support for ALETH42 keyboard layout

---
+++ keyboards/25keys/aleth42/keymaps/vial/config.h
@@ -0,0 +1,28 @@
+/* Copyright 2020 monksoffunk
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+// place overrides here
+// #define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
+#define VIAL_KEYBOARD_UID {0xA3, 0x67, 0x92, 0xE7, 0x06, 0x5C, 0x97, 0x07}
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+
+// VIAL unlock combo Q -> P
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0} 
+#define VIAL_UNLOCK_COMBO_COLS {1, 10}
