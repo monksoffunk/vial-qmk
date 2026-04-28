e6a88595284de4d96fc79b39356746022962536b
Monksoffunk <monksoffunk@users.noreply.github.com>
2026-04-29 01:16:39 +0900

    Add support for ALETH42 rev2.4 ortho keyboard
    - Updated README with maintainer and hardware information.
    - Created config.h for encoder hot swap functionality.
    - Added keyboard.json for rev2.4 specifications.
    - Implemented default keymap in keymap.c with multiple layers.
    - Introduced VIAL support with configuration files for VIAL keymap.
    - Added rules.mk to manage source files and encoder settings.
    - Implemented rev2_4_ortho.c for keyboard initialization and OS detection.
    - Updated 40_ortho_keymap.h to include new layer definitions.

--- keyboards/25keys/common/40_ortho_keymap.h
+++ keyboards/25keys/common/40_ortho_keymap.h
@@ -3,6 +3,7 @@
 #define LOWELNG2    LT(_LOWER, KC_LNG2)
 #define RAISLNG1    LT(_RAISE, KC_LNG1)
 #define EXTLEFT     LT(_EXTRA, KC_LEFT)
+#define ADJUST      MO(_ADJUST)
 
 /* Default Layer
  * ,-----------------------------------------------------------------------.
@@ -19,7 +20,7 @@
 #define __________40_ORTHO_DEFAULT_1__________ KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC
 #define __________40_ORTHO_DEFAULT_2__________ LCTLTAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT
 #define __________40_ORTHO_DEFAULT_3__________ KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT
-#define __________40_ORTHO_DEFAULT_4__________ KC_LCTL, KC_LALT, _ADJUST, KC_LGUI, LOWELNG2,KC_SPC,  KC_BSPC, RAISLNG1,EXTLEFT, KC_DOWN, KC_UP,   KC_RGHT
+#define __________40_ORTHO_DEFAULT_4__________ KC_LCTL, ADJUST,  KC_LALT, KC_LGUI, LOWELNG2,KC_SPC,  KC_BSPC, RAISLNG1,EXTLEFT, KC_DOWN, KC_UP,   KC_RGHT
 
 /* Lower Layer fn0
  * ,-----------------------------------------------------------------------.
