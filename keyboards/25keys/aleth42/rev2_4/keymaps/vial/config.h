5ce5312f708dc53f4568bbbbb5ae5ba4c2ca7d53
Monksoffunk <monksoffunk@users.noreply.github.com>
2026-06-05 16:41:41 +0900

    feat: add initial support for ALETH42 rev2.4 keyboard with keymaps and configuration files

---
+++ keyboards/25keys/aleth42/rev2_4/keymaps/vial/config.h
@@ -0,0 +1,28 @@
+/* Copyright 2026 monksoffunk
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+// place overrides here
+// #define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
+#define VIAL_KEYBOARD_UID {0xD7, 0xDC, 0x77, 0xCF, 0x16, 0xB5, 0x4E, 0x89}
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 10
+
+// VIAL unlock combo Q -> P
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0} 
+#define VIAL_UNLOCK_COMBO_COLS {1, 10}
---
+++ keyboards/25keys/aleth42/rev2_4/rev2_4.c
@@ -0,0 +1,21 @@
+#include "quantum.h"
+#include "../common/matrix_remap.h"
+#include "../common/os_key_mapping.h"
+
+extern matrix_row_t matrix[MATRIX_ROWS]; // debounced values
+
+void keyboard_post_init_kb(void) {
+    keyboard_post_init_user();
+}
+
+bool process_detected_host_os_kb(os_variant_t os_type) {
+    if (!process_detected_host_os_user(os_type)) {
+        return false;
+    }
+    return set_os_key_mapping(os_type);
+}
+
+void matrix_scan_kb(void) {
+    matrix_remap(matrix);
+    matrix_scan_user();
+}
