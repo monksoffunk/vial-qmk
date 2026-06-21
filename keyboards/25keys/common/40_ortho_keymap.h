fda622db8a04f9a3429a8c9de106ee2204ac66b5
Monksoffunk <monksoffunk@users.noreply.github.com>
2026-06-21 10:33:50 +0900

    Fix 40 ortho extra layer keycode
    Add an EXTRA momentary-layer alias and use it in the adjust layer instead of
    the raw _EXTRA layer enum. Also restore the default left-arrow key to KC_LEFT.

--- keyboards/25keys/common/40_ortho_keymap.h
+++ keyboards/25keys/common/40_ortho_keymap.h
@@ -4,6 +4,7 @@
 #define RAISLNG1    LT(_RAISE, KC_LNG1)
 #define EXTLEFT     LT(_EXTRA, KC_LEFT)
 #define ADJUST      MO(_ADJUST)
+#define EXTRA       MO(_EXTRA)
 
 /* Default Layer
  * ,-----------------------------------------------------------------------.
@@ -20,7 +21,7 @@
 #define __________40_ORTHO_DEFAULT_1__________ KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC
 #define __________40_ORTHO_DEFAULT_2__________ LCTLTAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT
 #define __________40_ORTHO_DEFAULT_3__________ KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT
-#define __________40_ORTHO_DEFAULT_4__________ KC_LCTL, ADJUST,  KC_LALT, KC_LGUI, LOWELNG2,KC_SPC,  KC_BSPC, RAISLNG1,EXTLEFT, KC_DOWN, KC_UP,   KC_RGHT
+#define __________40_ORTHO_DEFAULT_4__________ KC_LCTL, ADJUST,  KC_LALT, KC_LGUI, LOWELNG2,KC_SPC,  KC_BSPC, RAISLNG1,KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 
 /* Lower Layer fn0
  * ,-----------------------------------------------------------------------.
@@ -69,7 +70,7 @@
  */
 
 #define __________40_ORTHO_ADJUST_1___________ KC_MUTE, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11
-#define __________40_ORTHO_ADJUST_2___________ _EXTRA,  RGB_TOG, RGB_VAI, RGB_SAI, AG_NORM, RGB_TOG, RGB_HUI, AG_SWAP, RGB_SAI, RGB_VAI, _______, KC_F12
+#define __________40_ORTHO_ADJUST_2___________ EXTRA,  RGB_TOG, RGB_VAI, RGB_SAI, AG_NORM, RGB_TOG, RGB_HUI, AG_SWAP, RGB_SAI, RGB_VAI, _______, KC_F12
 #define __________40_ORTHO_ADJUST_3___________ KC_CAPS, RGB_MOD, RGB_VAD, RGB_SAD, _______, RGB_MOD, RGB_HUD, _______, RGB_SAD, RGB_VAD, _______, _______
 #define __________40_ORTHO_ADJUST_4___________ KC_SLEP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 
